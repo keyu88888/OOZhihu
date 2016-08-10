add4925aa67a9b526d7b720286358b3b81f14111
Keyu Chan <309065596@qq.com>
2016-08-10 16:12:08 +0800

    加入okhttp包

--- app/src/main/java/com/oo/zhihu/MainActivity.java
+++ app/src/main/java/com/oo/zhihu/MainActivity.java
@@ -3,11 +3,15 @@ package com.oo.zhihu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
     }
 }
